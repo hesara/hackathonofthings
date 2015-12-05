8e96fe97b2fa74bb710877f2af886994771a87fc
elmot <elmot@vaadin.com>
2015-12-05 23:02:49 +0200

    final Topic and try resources

--- src/main/java/org/vaadin/hackathonofthings/io/AbstractFileDataSource.java
+++ src/main/java/org/vaadin/hackathonofthings/io/AbstractFileDataSource.java
@@ -10,21 +10,21 @@ import org.vaadin.hackathonofthings.data.DataEvent;
 import org.vaadin.hackathonofthings.data.Topic;
 
 public abstract class AbstractFileDataSource extends AbstractDataSource {
-	private Topic topic;
+    private final Topic topic;
 
-	public AbstractFileDataSource(Topic topic) {
-		this.topic = topic;
-	}
-	
-	public void readFile(String filename) throws IOException {
-		Stream<String> lines = Files.lines(Paths.get(filename));
-		lines.map(this::convertLine).filter(event -> event != null).forEach(this::sendEvent);
-		lines.close();
-	}
-	
-	public Topic getTopic() {
-		return topic;
-	}
+    public AbstractFileDataSource(Topic topic) {
+        this.topic = topic;
+    }
 
-	protected abstract DataEvent convertLine(String line);
+    public void readFile(String filename) throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+            lines.map(this::convertLine).filter(event -> event != null).forEach(this::sendEvent);
+        }
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    protected abstract DataEvent convertLine(String line);
 }
